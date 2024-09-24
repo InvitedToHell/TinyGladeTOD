c9fd5b767271a7f5be075a3fab588632cd776832
InvitedToHell <BrianDETeam@gmail.com>
2024-09-24 17:33:11 +0200

    arrayBuffer() instead of text

--- functions/blob.js
+++ functions/blob.js
@@ -1,5 +1,5 @@
 export async function onRequest(context) {
   const { cid } = context.request.url.searchParams;
   const resp = await fetch(`https://ryeland.pouncelight.games/v1/blob/${cid}`);
-  return new Response(await resp.text());
+  return new Response(await resp.arrayBuffer());
 }
