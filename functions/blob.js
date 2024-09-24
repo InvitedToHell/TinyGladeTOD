4b8f9dc251cda0d890094abb1c6996ef22ade511
InvitedToHell <BrianDETeam@gmail.com>
2024-09-24 17:34:57 +0200

    return fetch

--- functions/blob.js
+++ functions/blob.js
@@ -1,5 +1,4 @@
 export async function onRequest(context) {
   const { cid } = context.request.url.searchParams;
-  const resp = await fetch(`https://ryeland.pouncelight.games/v1/blob/${cid}`);
-  return new Response(await resp.arrayBuffer());
+  return fetch(`https://ryeland.pouncelight.games/v1/blob/${cid}`);
 }
